bab42fc322ae864a883f6a80e37d2ef14a5b726e
slunzok <slunzok@linux.pl>
2018-05-16 14:37:39 +0200

    Add unique URLs for each list

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,5 +4,5 @@ from lists import views
 urlpatterns = [
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name="view_list")
+    re_path(r'^lists/(.+)/$', views.view_list, name="view_list")
 ]
