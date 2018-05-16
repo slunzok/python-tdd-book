f10e82c97f2ae28903a531efb0f0454c6384fdf1
slunzok <slunzok@linux.pl>
2018-05-16 15:35:50 +0200

    Add URLs for adding a new item to an existring list via POST

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,11 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
-    return render(request, 'lists/list.html', {'items': items})
+    return render(request, 'lists/list.html', {'list': list_})
