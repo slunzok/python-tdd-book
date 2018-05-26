44b95d704592fa3aaf4047486f6deb85f6b2fdef
slunzok <slunzok@linux.pl>
2018-05-26 15:14:48 +0200

    Add accounts app with custom User Model and Token Model to link emails with a unique ID

--- superlists/settings.py
+++ superlists/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
