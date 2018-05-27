41e0572880b074e4d3a60e2a778475aa42e46a0b
slunzok <slunzok@linux.pl>
2018-05-27 15:19:23 +0200

    Add test for logout link

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import logout
 from django.urls import path, re_path
 from accounts import views
 
 urlpatterns = [
     re_path('^send_login_email$', views.send_login_email, name='send_login_email'),
     re_path('^login$', views.login, name='login'),
+    re_path('^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
