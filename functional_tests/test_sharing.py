659f3a00d1f527dd50ec91e14c53142fc082bedf
slunzok <slunzok@linux.pl>
2018-06-03 12:55:21 +0200

    Create Page objects for list pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListsPage
 
 def quit_if_possible(browser):
     try: browser.quit()
@@ -19,12 +21,28 @@ class SharingTest(FunctionalTest):
 
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('Get help')
+        list_page = ListPage(self).add_list_item('Get help')
 
-        share_box = self.browser.find_element_by_css_selector(
-            'input[name="sharee"]'
-        )
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        list_page.share_list_with('oniciferous@example.com')
+
+        self.browser = oni_browser
+        MyListsPage(self).go_to_my_lists_page()
+
+        self.browser.find_element_by_link_text('Get help').click()
+
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_owner(),
+            'edith@example.com',
+        ))
+
+        list_page.add_list_item('Hi Edith!')
+
+        self.browser = edith_browser
+        self.browser.refresh()
+        list_page.wait_for_row_in_list_table('Hi Edith!', 2)
